6cb5229a9161af80f2d2995946f14a3668bfc03b
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-21 05:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 /* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer = "leo";
+let favoriteCustomer = "someone"
+function upperCaseCustomerName(){
+  uppercasename = customerName.toUpperCase();
+  return uppercasename;
+}
+function setBestCustomer(){
+   bestCustomer = "not bob";
+}
+function overWriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "newone";
+}
+function attemptTwoFavoriteCustomers(){
+let  favoriteCustomer = "people";
+}
