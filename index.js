4d0d80c84d16449cd38c26b33c1261aaeeeff0b0
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-21 05:34:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 /* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer = "leo";
+let favoriteCustomer = "someone"
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+   bestCustomer = "not bob";
+}
+function overWriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "newone";
+}
+function attemptTwoFavoriteCustomers(){
+let  favoriteCustomer = "people";
+
+throw new SyntaxError('err', 'blah.js',1);
+}
